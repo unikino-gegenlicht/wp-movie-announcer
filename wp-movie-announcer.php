ab0b51bbf504ef134dedef2478a9e39b0468aa22
Jan Eike Suchard <60288203+dr4hcu5-jan@users.noreply.github.com>
2026-04-22 09:18:06 +0200

    refactor: moved the activation and deactivation hooks into separate file

--- wp-movie-announcer.php
+++ wp-movie-announcer.php
@@ -53,35 +53,6 @@ add_action( "wp_ajax_wpma_test", "wpma_test" );
 add_action( "wp_ajax_wpma_publish_manually", "wpma_publish_manually" );
 add_action( "wpma_publish_screenings", "wpma_publish_screenings" );
 
-/**
- * Activation Hook
- *
- * This function automatically schedules the publishing cycle to the next
- * sunday at 16:00h.
- *
- * @return void
- * @throws Exception
- */
-function wpma_activate() {
-	if ( ! wp_next_scheduled( 'wpma_publish_screenings' ) ) {
-		$tz              = new DateTimeZone( 'Europe/Berlin' );
-		$next_sunday     = new DateTimeImmutable( "next Sunday", $tz );
-		$initial_post_ts = $next_sunday->setTime( 16, 0, 0 )->getTimestamp() + $next_sunday->getOffset();
-		wp_schedule_event( $initial_post_ts, 'weekly', 'wpma_publish_screenings' );
-	}
-}
-
-/**
- * Deactivation Hook
- *
- * This function removes the next scheduled publishing
- *
- * @return void
- */
-function wpma_deactivate() {
-	$next_publish = wp_next_scheduled( 'wpma_publish_screenings' );
-	wp_unschedule_event( $next_publish, 'wpma_publish_screenings' );
-}
 
 function wpma_enqueue_scripts() {
 	wp_enqueue_script( 'wpma-ajax', plugin_dir_url( __FILE__ ) . 'js/ajax.js', [ 'jquery' ], md5_file( plugin_dir_path( __FILE__ ) . "js/ajax.js" ), false );
