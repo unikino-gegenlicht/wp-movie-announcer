753b9060b23784189e8f56ba74ac7e03d817bfe7
Jan Eike Suchard <60288203+dr4hcu5-jan@users.noreply.github.com>
2026-04-22 09:32:27 +0200

    refactor: moving functionality for discord into seperate file

--- wp-movie-announcer.php
+++ wp-movie-announcer.php
@@ -24,18 +24,14 @@
 
 defined( 'ABSPATH' ) || exit;
 
-use PhpChannels\DiscordWebhook\Discord;
-
-
-
-
-
 
 const WPMA_ATPROTO_MAX_CHAR_COUNT = 300;
 
 require_once 'vendor/autoload.php';
 require_once "src/settings.php";
 require_once "src/wp-hooks.php";
+require_once "src/mastodon.php";
+require_once "src/discord.php";
 
 register_activation_hook( __FILE__, "wpma_activate" );
 register_deactivation_hook( __FILE__, "wpma_deactivate" );
@@ -59,7 +55,6 @@ function wpma_enqueue_scripts() {
 		'publishNonce' => wp_create_nonce( "wpma_publish_manually" ),
 	] );
 }
-
 
 function wpma_publish_manually(): void {
 	check_ajax_referer( "wpma_publish_manually" );
@@ -105,57 +100,6 @@ function wpma_test() {
 	}
 }
 
-
-
-/**
- * This function creates a private test post in the configured Bluesky/ATProto
- * instance
- *
- *
- * @return void
- */
-function wpma_test_atproto(): void {
-	$instance_url = rwmb_meta( "wpma_mastodon_instance_url", [ "object_type" => "setting" ], WPMA_OPTION_NAME );
-	if ( mb_trim( $instance_url ) === "" ) {
-		throw new ValueError( "No mastodon instance URL set.", 1 );
-	}
-	$access_token = rwmb_meta( "mastodon_access_token", [ "object_type" => "setting" ], WPMA_OPTION_NAME );
-	if ( mb_trim( $access_token ) === "" ) {
-		throw new ValueError( "No mastodon access token set.", 2 );
-	}
-}
-
-
-function wpma_test_discord(): void {
-	$webhook_url = rwmb_meta( "discord_webhook_url", [ "object_type" => "setting" ], WPMA_OPTION_NAME );
-	if ( mb_trim( $webhook_url ) === "" ) {
-		throw new ValueError( "No discord webhook URL set.", 3 );
-	}
-
-	$content = "# Testnachricht" . PHP_EOL;
-	$content .= "Aktuelle Kalenderwoche: " . new DateTimeImmutable( "now", new DateTimeZone( "Europe/Berlin" ) )->format( "W" ) . PHP_EOL;
-	$content .= "## Filmtitel 1";
-
-	try {
-		$msg = Discord::message( $webhook_url );
-		$msg->setUsername( "Unikino GEGENLICHT" );
-		$msg->setContent( $content );
-		$msg->setTitle( "Unikino GEGENLICHT" );
-		$msg->setImage( plugin_dir_url( __FILE__ ) . "static/demo.jpg" );
-		$msg->setThumbnail( plugin_dir_url( __FILE__ ) . "static/demo.jpg" );
-		$msg->setDescription( "test" );
-		$msg->setAuthor( wp_get_current_user()->display_name, wp_get_current_user()->user_url, get_avatar_url( wp_get_current_user()->user_email ) );
-		$msg->send();
-	} catch ( Exception $e ) {
-		wp_send_json_error( [ "error" => $e->getMessage() ] );
-	}
-}
-
-/**
- * Publish the screening schedule on the social medias
- *
- * @return void
- */
 function wpma_publish_screenings() {
 	$posts = wpma_get_publishable_posts();
 
@@ -178,75 +122,6 @@ function wpma_publish_screenings() {
 	}
 }
 
-
-
-
-function wpma_publish_discord( array $posts ): void {
-	$webhook_url = rwmb_meta( "discord_webhook_url", [ "object_type" => "setting" ], WPMA_OPTION_NAME );
-	if ( mb_trim( $webhook_url ) === "" ) {
-		throw new ValueError( "No mastodon instance URL set.", 1 );
-	}
-	$next_monday = new DateTimeImmutable( "next Monday" );
-	$next_sunday = $next_monday->add( new DateInterval( "P6D" ) );
-
-	$content = "# Das Programm in der KW {$next_monday->format('W')} ({$next_monday->format('d.m.')}–{$next_sunday->format('d.m.')})" . PHP_EOL;
-	$content .= "Auch für die KW {$next_monday->format('W')} haben wir euch wieder einige Vorstellungen mitgebracht";
-
-	$msg = Discord::message( $webhook_url );
-	$msg->setUsername( "Unikino GEGENLICHT" );
-	$msg->setAvatarUrl( get_site_icon_url() );
-	$msg->setContent( $content );
-	$msg->send();
-
-	foreach ( $posts as $post ) {
-		$enforce_anonymized = ggl_get_licensing_type( $post->ID ) != "full";
-
-		$title               = ggl_get_localized_title( $post, $enforce_anonymized );
-		$original_title      = ggl_get_title( $post, $enforce_anonymized );
-		$summary             = ggl_get_summary( $post, $enforce_anonymized );
-		$image_url           = ggl_get_feature_image_url( $post, force_anonymized: $enforce_anonymized );
-		$url                 = get_post_permalink( $post );
-		$masked_url          = str_replace( "https://", "", $url );
-		$reservations        = ggl_get_event_booking_url( $post );
-		$masked_reservations = str_replace( "https://", "", $reservations );
-		$proposers           = ggl_get_proposers( $post );
-		$proposer_names      = array_map( function ( $item ) {
-			return ggl_get_title( $item );
-		}, $proposers );
-
-		if ( count( $proposer_names ) == 1 && $proposer_names[0] !== "" ) {
-			$author = array_pop( $proposer_names );
-		} elseif ( count( $proposer_names ) > 1 ) {
-			$last_name = array_pop( $proposer_names );
-			$author    = join( ", ", $proposer_names );
-			$author    .= " und " . $last_name;
-		} else {
-			$author = "uns";
-		}
-
-		$content = mb_trim( "## {$title} " . ( $title !== $original_title ? "(OT: {$original_title})" : "" ) ) . PHP_EOL;
-		$content .= mb_trim( strip_tags( str_replace( "</p>", PHP_EOL . PHP_EOL, $summary ) ) ) . PHP_EOL;
-		$content .= PHP_EOL;
-		$content .= "Und warum der Film aus der Sicht von {$author} für euch sehenswert ist erfahrt ihr unter [{$masked_url}]({$url}?utm_source=discord.com&utm_medium=social&utm_campaign=social-announcements&utm_content=textlink)" . PHP_EOL;
-		$content .= PHP_EOL;
-		if ( $reservations !== "" ) {
-			$content .= "🎟️ Reservierungen für diese Vorstellung sind unter [{$masked_reservations}]({$reservations}?utm_source=discord.com&utm_medium=social&utm_campaign=social-announcements&utm_content=textlink) möglich." . PHP_EOL;
-		}
-
-		$msg = Discord::message( $webhook_url );
-		$msg->setContent( mb_trim( $content ) );
-		$msg->setUsername( "Unikino GEGENLICHT" );
-		$msg->setAvatarUrl( get_site_icon_url() );
-		$msg->setUrl( "{$url}?utm_source=discord.com&utm_medium=social&utm_campaign=social-announcements&utm_content=textlink" );
-		$msg->setTitle( "{$title} " . ( $title !== $original_title ? "(OT: {$original_title})" : "" ) );
-		$msg->setImage( $image_url );
-
-
-		send_discord_msg:
-		sleep( 2 );
-		$msg->send();
-	}
-}
 
 /**
  * @return WP_Post[]|int[]
